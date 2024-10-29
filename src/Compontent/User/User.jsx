c08e3330b0cc57748c0387f6add8b122124aceea
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-29 12:02:46 +0600

    single user

---
+++ src/Compontent/User/User.jsx
@@ -0,0 +1,12 @@
+
+
+const User = ({user}) => {
+    console.log(user)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default User;
--- src/Compontent/Users/Users.jsx
+++ src/Compontent/Users/Users.jsx
@@ -1,10 +1,17 @@
 import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
 
 const Users = () => {
     const users =useLoaderData()
     return (
         <div>
-            <h1>Users {users.length}</h1>
+            <h1 className="text-2xl font-semibold">Users {users.length}</h1>
+            <h1></h1>
+            <div>
+                {
+                    users.map((user,idx) =><User key={idx} user={user}></User>)
+                }
+            </div>
         </div>
     );
 };
