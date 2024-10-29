6cde17a86f4c99be1a37951cec5e499e6b5ce695
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-29 10:39:48 +0600

    data path

--- src/Compontent/Home/Home.jsx
+++ src/Compontent/Home/Home.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <h1>This is the home compontent</h1>
             <Outlet></Outlet>
         </div>
