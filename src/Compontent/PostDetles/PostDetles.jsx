b397839bb76a017e0b6bcb0c61dde91d523af4b2
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-30 23:58:33 +0600

    go back

--- src/Compontent/PostDetles/PostDetles.jsx
+++ src/Compontent/PostDetles/PostDetles.jsx
@@ -1,15 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetles = () => {
     const postDetles = useLoaderData();
-    const { body, title, id } = postDetles
-    console.log(postDetles)
+    const { body, title, id } = postDetles;
+    const naveget =useNavigate()
+    const hendleGoBack =() =>{
+        naveget(-1)
+    }
     return (
         <div className="w-6/12 mx-auto my-8">
             <div className="flex flex-col justify-center items-center text-center bg-amber-900 p-5 rounded-xl shadow-lg">
                 <h1 className="text-5xl font-bold ">{id}</h1>
                 <p className="text-2xl font-semibold">{title}</p>
                 <p>{body}</p>
+                <button className="btn" onClick={hendleGoBack}>Go Back</button>
             </div>
         </div>
     );
