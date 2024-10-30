5766cbb2fc6dabe89778525704f93e5b0d953b19
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-31 00:24:58 +0600

    loader

--- src/Compontent/Home/Home.jsx
+++ src/Compontent/Home/Home.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { HashLoader } from "react-spinners";
 
 const Home = () => {
+    const useNeveget =useNavigation();
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                useNeveget.state==='loading'?<HashLoader className="text-center w-5/12 mx-auto text-white my-44" />:<Outlet></Outlet>
+            }
+            
             <Footer></Footer>
         </div>
     );
